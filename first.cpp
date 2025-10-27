2126d815cfd95cb515d2d0d2b41421fa52ddd876
angel <angelinantonova@icloud.com>
2025-10-27 11:29:34 +0300

    ddd

---
+++ first.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+/*
+int main(){
+double a;
+double p;
+double s;
+std::cout<<"enter side of square: ";
+std::cin>>a;
+p = 4*a;
+s = a*a;
+std::cout<<"perimetr: " << p << "\nSquare:" << s <<std::endl;
+
+return 0;
+} 
+*/
+
+int main(){
+double a;
+double b;
+
+
+
