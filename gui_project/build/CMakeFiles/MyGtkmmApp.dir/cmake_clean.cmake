f987b5c13e79ba2c7ea909f0902248e7a7a9c2e5
ilia <gigolaevila@gmail.com>
2026-05-05 10:16:52 +0300

    rmrkHFHJ4

---
+++ gui_project/build/CMakeFiles/MyGtkmmApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyGtkmmApp.dir/main.cpp.o"
+  "CMakeFiles/MyGtkmmApp.dir/main.cpp.o.d"
+  "CMakeFiles/MyGtkmmApp.dir/texteditor.cpp.o"
+  "CMakeFiles/MyGtkmmApp.dir/texteditor.cpp.o.d"
+  "MyGtkmmApp"
+  "MyGtkmmApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyGtkmmApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gui_project/build/CMakeFiles/MyGtkmmApp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MyGtkmmApp.
+# This may be replaced when dependencies are built.
