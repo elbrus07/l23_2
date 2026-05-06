9930c342c00985f179bda0b362020f71d7214299
Elbrus <eolisaev@yandex.ru>
2026-05-06 06:34:44 +0000

    d

---
+++ gui_project/main.cpp
@@ -0,0 +1,11 @@
+#include "hello.h"
+#include <gtkmm/application.h>
+
+
+int main(int argc, char* argv[])
+{
+  auto app = Gtk::Application::create("org.gtkmm.examples.base");
+
+  return app->make_window_and_run<HelloWorld>(argc, argv);
+}
+
