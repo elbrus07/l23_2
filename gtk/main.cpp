ac1886139da62308a1e8679fe169076a385530a8
zerkib <dima.karaev.07@mail.ru>
2026-05-25 23:00:13 +0300

    Update main.cpp

--- gtk/main.cpp
+++ gtk/main.cpp
@@ -2,6 +2,9 @@
 
 int main(int argc, char* argv[])
 {
+    g_setenv("GSK_RENDERER", "cairo", TRUE);
+    g_setenv("GDK_DEBUG", "gl-disabled", TRUE);
+
     auto app = Gtk::Application::create("org.example.textredactor");
     return app->make_window_and_run<TextRedactor>(argc, argv);
 }
