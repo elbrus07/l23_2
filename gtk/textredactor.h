5036ab29fcf724b6ce4965f16ab4deef9121813a
zerkib <dima.karaev.07@mail.ru>
2026-06-08 15:18:48 +0300

    Update textredactor.h

--- gtk/textredactor.h
+++ gtk/textredactor.h
@@ -12,50 +12,48 @@ public:
     ~TextRedactor();
 
 private:
-    void on_menu_file_new();
-    void on_menu_file_open();
-    void on_menu_file_save();
-    void on_menu_file_save_as();
-    void on_menu_file_quit();
-    
-    void on_font_button_clicked();
-    void on_font_size_changed();
-    void apply_font_to_all_text();
-    void update_font_size_combo();
+
+    void create_click();
+    void open_click();
+    void save_click();
+
+    void load_file(const std::string& path);
+    void save_file(const std::string& path);
     
     void setup_tags();
     void highlight_words();
     void on_buffer_changed();
     
-    void load_file(const std::string& path);
-    void save_file(const std::string& path);
-
+    // Новые методы для работы со шрифтами
+    void on_font_click();
+    void apply_font(const std::string& font_desc);
+    void update_font_button_label();
+    
 private:
+
     Gtk::Box main_box;
-    Gtk::Box top_bar;
+    Gtk::Box button_box;
+    Gtk::Box font_box;  // Новая панель для шрифтов
+    Gtk::Button create_btn;
+    Gtk::Button open_btn;
+    Gtk::Button save_btn;
+    Gtk::Button font_btn;  // Кнопка выбора шрифта
     Gtk::ScrolledWindow scroll_win;
     Gtk::TextView text_view;
-    Gtk::MenuButton file_menu_btn;
-    
-    Glib::RefPtr<Gio::Menu> m_menu_file;
-    
-    Gtk::Button font_btn;
-    Gtk::ComboBoxText size_combo;
-    Glib::RefPtr<Gtk::TextTag> font_tag;
-    std::string font_family;
-    int font_size;
-    
+
     Glib::RefPtr<Gtk::TextBuffer> text_buffer;
-    Glib::RefPtr<Gtk::TextTag> red_tag;
+    Glib::RefPtr<Gtk::TextTag> red_tag;      // Тег для красного цвета
+    Glib::RefPtr<Gtk::TextTag> font_tag;     // Тег для шрифта
+
+    std::string current_file;  // Текущий файл
     
-    std::string current_file;
-    Glib::RefPtr<Gtk::FileDialog> file_dialog;
-    
+    std::string current_font_description;  // Текущее описание шрифта
+
+    // Список слов для подсветки 
     std::vector<std::string> words_to_highlight = {
         "error", "bug", "fixme", "todo", "warning", 
         "красный", "важно", "ошибка", "баг"
     };
-    
     bool highlight_enabled;
 };
 
