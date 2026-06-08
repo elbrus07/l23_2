f52b496628f1a386a44f4ebf6dd92fa6e8db9cf0
zerkib <dima.karaev.07@mail.ru>
2026-06-08 14:59:46 +0300

    Update textredactor.h

--- gtk/textredactor.h
+++ gtk/textredactor.h
@@ -11,44 +11,51 @@ public:
     TextRedactor();
     ~TextRedactor();
 
-
-
 private:
-
-    void create_click();
-    void open_click();
-    void save_click();
-
-    void load_file(const std::string& path);
-    void save_file(const std::string& path);
+    void on_menu_file_new();
+    void on_menu_file_open();
+    void on_menu_file_save();
+    void on_menu_file_save_as();
+    void on_menu_file_quit();
+    
+    void on_font_button_clicked();
+    void on_font_size_changed();
+    void apply_font_to_all_text();
+    void update_font_size_combo();
     
     void setup_tags();
     void highlight_words();
     void on_buffer_changed();
     
-    private:
-   
+    void load_file(const std::string& path);
+    void save_file(const std::string& path);
 
+private:
     Gtk::Box main_box;
-    Gtk::Box button_box;
-    Gtk::Button create_btn;
-    Gtk::Button open_btn;
-    Gtk::Button save_btn;
+    Gtk::Box top_bar;
     Gtk::ScrolledWindow scroll_win;
     Gtk::TextView text_view;
-
-
+    Gtk::MenuButton file_menu_btn;
+    
+    Glib::RefPtr<Gio::Menu> m_menu_file;
+    
+    Gtk::Button font_btn;
+    Gtk::ComboBoxText size_combo;
+    Glib::RefPtr<Gtk::TextTag> font_tag;
+    std::string font_family;
+    int font_size;
+    
     Glib::RefPtr<Gtk::TextBuffer> text_buffer;
-    Glib::RefPtr<Gtk::TextTag> red_tag;  // Тег для красного цвета
-
-
-    std::string current_file;  // Текущий файл
-
-    // Список слов для подсветки 
+    Glib::RefPtr<Gtk::TextTag> red_tag;
+    
+    std::string current_file;
+    Glib::RefPtr<Gtk::FileDialog> file_dialog;
+    
     std::vector<std::string> words_to_highlight = {
         "error", "bug", "fixme", "todo", "warning", 
         "красный", "важно", "ошибка", "баг"
     };
+    
     bool highlight_enabled;
 };
 
